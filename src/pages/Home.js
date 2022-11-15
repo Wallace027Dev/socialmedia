0bad0320869f34b5e0854f45edd7bae7ce8ec51a
Wallace Vieira <wallaceofc@hotmail.com>
2022-11-14 21:33:04 -0300

    Consumo de API com React Adicionando projeto ao Github, conectando a API

---
+++ src/pages/Home.js
@@ -0,0 +1,49 @@
+import { useState, useEffect } from "react";
+
+import Feed from '../components/Feed';
+import PostForm from '../components/PostForm';
+
+export default function Home() {
+  const [posts, setPosts] = useState([]);
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(()=>{
+    fetch('http://localhost:3001/posts')
+    .then(async (response)=>{
+      const body = await response.json();
+
+      setPosts(body.map((post)=>({
+        ...post,
+        publishedAt: new Date(post.publishedAt)
+      })));
+      setIsLoading(false);
+    })
+  },[])
+
+  function handleSubmit({ history, userName }) {
+    setPosts([
+      ...posts,
+      {
+        id: Math.random(),
+        content: history,
+        userName,
+        publishedAt: new Date(),
+      },
+    ]);
+  }
+
+  return (
+    <>
+      <PostForm onSubmit={handleSubmit} />
+      
+      <main>
+        <Feed
+        isLoading={isLoading}
+          posts={posts}
+          title="Seu Feed"
+          subtitle="Acompanhe o que seus amigos estão pensando em tempo real"
+        />
+      </main>
+    </>
+  );
+}
